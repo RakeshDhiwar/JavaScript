ed6011da7fd8f640c927591fdef050c07e574d62
Rakesh Kumar Dhiwar <134353813+RakeshDhiwar@users.noreply.github.com>
2023-11-11 04:31:15 +0000

    variables

---
+++ JS_Basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 1222
+let accountEmail = "rk@google.com"
+var accountPassword = "12390"
+accountCity = "Raipur"
+
+// accountId = "12"
+// accountPassword = "rk"
+console.table([accountId,accountPassword])
