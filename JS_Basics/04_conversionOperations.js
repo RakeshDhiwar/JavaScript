701e5e90915d08c7d17dc97a729fffd7f98a8e59
Rakesh Kumar Dhiwar <134353813+RakeshDhiwar@users.noreply.github.com>
2023-11-15 08:21:45 +0000

    conversion operations

---
+++ JS_Basics/04_conversionOperations.js
@@ -0,0 +1,24 @@
+//  Conversion Operation
+
+// let value = 5
+// let negValue = -value
+// console.log(negValue);
+
+// let str1 = "hello"
+// let str2 = " Rakesh"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+2);       //12
+// console.log(1+"2");       //12
+// console.log("1"+"2");     //12
+// console.log("1"+ 2 + 2);  //122
+// console.log(1 + 2 + "2"); //32
+
+// console.log(true+);      //error
+console.log(+"");           //0
+
+
+let x = 3;
+let y = ++x;
+console.log(x,y);
