c634dae231fd4bf0b7d7f5e4c1af688cbbceb62d
Rakesh Kumar Dhiwar <134353813+RakeshDhiwar@users.noreply.github.com>
2023-11-20 05:12:26 +0000

    Number and Math Library in JavaScript

---
+++ JS_Basics/08_Number_Math.js
@@ -0,0 +1,42 @@
+// const score = 240
+
+// const balance = new Number(100)
+// console.log(typeof balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2)); //After decimal two digits
+
+// const num = 24234.2848274
+// console.log(num.toPrecision(3));
+
+// const hundreds = 100000000
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// console.log(Number.MAX_SAFE_INTEGER)
+// console.log(Number.EPSILON)
+// console.log(Number.MAX_VALUE)
+// console.log(Number.MIN_VALUE)
+// console.log(Number.NEGATIVE_INFINITY)
+
+
+
+//+++++++++++++++++++++++++++++++++++ MATHS ++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-3));
+// console.log(Math.round(35.4999));
+// console.log(Math.ceil(35.4999));
+// console.log(Math.floor(35.4999));
+// console.log(Math.sqrt(49));
+// console.log(Math.min(3,54,6,7));
+// console.log(Math.max(3,54,6,7));
+
+// console.log((Math.random()*10) + 1);
+// console.log((Math.random()*10).toPrecision(1));
+
+// Specifying Range in Math.Random
+min = 1; 
+max = 6;
+result = Math.floor(Math.random() * (max - min + 1)) + min;
+console.log(result);
