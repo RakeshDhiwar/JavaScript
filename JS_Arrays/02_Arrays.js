210ffcb9865527d6387ca4a4aae82d77ae4ff118
Rakesh Kumar Dhiwar <134353813+RakeshDhiwar@users.noreply.github.com>
2023-12-02 05:18:33 +0000

    spread , concat, Arrays. isArray, from and of

---
+++ JS_Arrays/02_Arrays.js
@@ -0,0 +1,27 @@
+// const marvel_heroes = ["Thor","Thanos", "BlackPanther"]
+// const dc_heroes = ["Flash","Batman", "Superman"]
+
+// marvel_heroes.push(dc_heroes)
+//Arrays take any values in this case whole dc_heroes arrays is inserted as a element...
+
+// console.log(marvel_heroes.concat(dc_heroes)); //concat returns a new array
+
+//Spread operator
+// const all_heroes = [...marvel_heroes,...dc_heroes]
+// console.log(all_heroes);
+
+// console.log(marvel_heroes);
+
+// const nums = [1,2,3,[4,5,6],7,[8,[9,10]]]
+// const real_nums = nums.flat(Infinity)
+
+// console.log(real_nums);
+
+// console.log(Array.isArray("Rakesh"));
+console.log(Array.from({name:"Rakesh",age:20}));
+
+let score1 = 100
+let score2 = 101
+let score3 = 102
+console.log(Array.of(score1,score2,score3));
+
