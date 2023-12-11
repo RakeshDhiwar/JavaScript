291024f7aa809fa9ad8b3bc8d78b40c4f146c20a
Rakesh Kumar Dhiwar <134353813+RakeshDhiwar@users.noreply.github.com>
2023-12-11 11:19:07 +0000

    scope in javaScript and diff b/w var and let

---
+++ JS_Basics/10_Scope.js
@@ -0,0 +1,12 @@
+
+
+
+if(true){
+    let a = 10
+    const b = 20
+    var c = 30
+}
+
+// console.log(a);  //give error of scope
+// console.log(b); //give error of scope
+// console.log(c); // this can be printed outside the scope which is problem
