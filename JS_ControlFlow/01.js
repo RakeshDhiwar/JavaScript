bf5c0dd0766542b19ff286cda823144f4f5e8cde
Rakesh Kumar Dhiwar <134353813+RakeshDhiwar@users.noreply.github.com>
2024-03-14 17:20:56 +0000

    Control flow in Javascript

---
+++ JS_ControlFlow/01.js
@@ -0,0 +1,36 @@
+//if
+// if(10==10==1){
+//     console.log("true");
+// }
+// console.log("hello");
+
+// if (2==="2"){
+//     console.log("true");
+// }
+const balance = 1000
+
+// if(balance > 500) console.log("test");
+
+// if (balance < 500) {
+//     console.log("less than");
+// } else if (balance < 750){
+//     console.log("less than 750");
+// } else {
+//     console.log("1000");
+// }
+
+// if (userloggedIn && debitcard) {
+//     console.log("Allowed");
+// }
+// if (logInfromgoogle || logInfromemail) {
+//     console.log("user log in ");
+// }
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
