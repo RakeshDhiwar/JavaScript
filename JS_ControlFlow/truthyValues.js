5e5aca209310eaf5e71279c355d281f6b35c3b65
Rakesh Kumar Dhiwar <134353813+RakeshDhiwar@users.noreply.github.com>
2024-03-14 17:21:29 +0000

    truthy and falsy values in JS

---
+++ JS_ControlFlow/truthyValues.js
@@ -0,0 +1,38 @@
+// let userEmail = "rk@dhiwar.cse"  //true
+// let userEmail = "" //false
+// let userEmail = []
+
+// if (userEmail) {
+//     console.log("Got Email");
+// } else{
+//     console.log("Don't have email");
+// }
+
+// FALSY VALUES
+// false, 0, -0, 'BigInt 0n', "", null , undefined, NaN
+
+//TRUTHY VALUES
+// rest all ,"0", 'false', " ", [], {}, function(){}
+
+// if (userEmail.length === 0) {
+//     console.log("Array is empty");
+// }
+
+// const emptyObj = {}
+
+// if (Object.keys(emptyObj).length === 0) {
+//     console.log("Object is empty");
+// }
+
+// Nullish Coalescing Operator (??): null undefined
+
+// let val1;
+// // val1 = 5 ?? 10
+// val1 = null ?? 10
+
+// console.log(val1);
+
+
+
+// Terniary Operator
+// condition ? true : false
